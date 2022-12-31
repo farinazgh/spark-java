4be3237035eb6c596148d7f044939f93537082a8
Ghasemi Farinaz <farinaz.ghasemi@acast.com>
2022-12-31 14:30:23 +0100

    schema manipulation

--- src/main/java/com/polaris/spark/PrettyFormatter.java
+++ src/main/java/com/polaris/spark/PrettyFormatter.java
@@ -9,11 +9,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Pretty formatter for a JDBC resultset
- * 
- * @author jgp
- */
 public class PrettyFormatter {
   private static Logger log =
       LoggerFactory.getLogger(PrettyFormatter.class);
